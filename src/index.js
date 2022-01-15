0d07cb9ffc52a3dcbb65550042d472a1819fbfbe
Aaron Elias <aaronelias007@gmail.com>
2022-01-14 20:16:32 -0500

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+const form = document.querySelector("#create-task-form")
+const taskList = document.querySelector("#tasks")
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+  console.log(e)
+  const task = e.target.new_task_description.value
+  const li = document.createElement("li")
+  li.innerText=task
+  taskList.append(li)
+})
